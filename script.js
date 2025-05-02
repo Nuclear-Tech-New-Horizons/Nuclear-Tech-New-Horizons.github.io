f16e63c31b74d4155bf75072cb4b277dbaea8632
Bufka Second <4129261@school.ecsd.net>
2025-05-02 18:18:05 +0000

    a

--- script.js
+++ script.js
@@ -1,17 +1,10 @@
 async function switchLanguage(lang) {
-    try {
-      const response = await fetch(`download/${lang}.json`);
-      if (!response.ok) {
-        throw new Error(`Failed to load ${lang}.json`);
-      }
+  try {
+      const response = await fetch(`${lang}.json`);
       const data = await response.json();
       document.getElementById('tagline').textContent = data.tagline;
       document.getElementById('description').textContent = data.description;
-    } catch (error) {
-      console.error('Error switching language:', error);
-    }
+  } catch (error) {
+      console.error('Ошибка загрузки перевода:', error);
   }
-  
-  window.onload = () => {
-    switchLanguage('en');
-  };
+}
